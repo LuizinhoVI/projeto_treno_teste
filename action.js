cdc6b307e73c2124f6b17e9d1f459acbe8e4561e
LUIZ CODE <beat.luiz23@hotmail.com>
2023-10-02 12:03:02 -0300

    atualização de dados

--- action.js
+++ action.js
@@ -1,6 +1,13 @@
 function voltar(){
     window.location = "index.html";
 }
+function voltar_pro_exercicio(){
+    window.location = "../../Meus_exercicios.html";
+}
+
+function voltar_pro_treino(){
+    window.location = "/treinos/abdominal/abdominal.html";
+}
 function cronometro(){
     window.location ="cronometro.html";
 }
@@ -10,3 +17,12 @@ function add_caderno(){
 function meusexercicios(){
     window.location ="Meus_exercicios.html";
 }
+
+function abdominal(){
+window.location ="/treinos/abdominal/abdominal.html"
+}
+function abdominais(){
+    window.location ="/treinos/abdominal/abdominais.html"
+    
+    
+    }
